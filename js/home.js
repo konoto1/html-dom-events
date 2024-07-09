33b39c3072ff78d721006c69bfc65cd36cbc816d
Tomas.K <tomas.konopackis@gmail.com>
2024-07-09 16:13:19 +0300

    updated with introduction on button click

--- js/home.js
+++ js/home.js
@@ -7,3 +7,20 @@ function updateText() {
 
 buttonDOM.addEventListener('click', updateText);
 
+const introductionDOM = document.getElementsByClassName('introduction')[0];
+const btnDOM1 = introductionDOM.getElementsByTagName('button')[0];
+const btnDOM2 = introductionDOM.getElementsByTagName('button')[1];
+const btnDOM3 = introductionDOM.getElementsByTagName('button')[2];
+const btnDOM4 = introductionDOM.getElementsByTagName('button')[3];
+const paragraphDOM = document.getElementById('paragraph');
+
+
+
+function introduction(n = '_____') {
+    paragraphDOM.insertAdjacentHTML('afterBegin', `<p>Hello, my name is ${n}</p>`);
+}
+
+btnDOM1.addEventListener('click', () => introduction(btnDOM1.innerText));
+btnDOM2.addEventListener('click', () => introduction(btnDOM2.innerText));
+btnDOM3.addEventListener('click', () => introduction(btnDOM3.innerText));
+btnDOM4.addEventListener('click', () => introduction(btnDOM4.innerText));
